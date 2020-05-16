9eb8a83f7a2c76b0f9235581afee2767e9ba4ad9
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-16 16:41:40 +0530

    add json web token

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,30 @@
+// this file contain authentication part of route
+const passport =require('passport');
+
+const JWTStrategy=require('passport-jwt').Strategy;
+
+const ExtractJWT=require('passport-jwt').ExtractJwt;
+
+const User=require('../models/doctor');
+
+let opts={
+    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken();
+    secretOrKey='hospitalApi'
+}
+
+passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
+
+    User.findById(jwtPayLoad._id,function(err,user){
+        if(err){console.log('Error in finding user from JWT'); return;}
+
+        if(user){
+            return done(null,user);
+
+        }else{
+            return done(null,false);
+        }
+
+    });
+}));
+
+module.exports=passport;
--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express=require('express');
 const app=express();
 
 const port=8000;
+const passport=require('passport');
+const passportJwt=require('passport-jwt');
+const bodyParser=require('body-parser');
+// connect to mongodb
+const db=require('./config/mongoose');
+
+app.use(passport.initialize());
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 // app.use('/',require('./routes'));
 
